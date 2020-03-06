8fd70521e693fc725247b9d79360248937905b73
Mae Ann Bartolaba <maeannbartolaba05@gmail.com>
2020-03-06 12:03:25 +0800

    Create Multiplication.cpp

---
+++ Multiplication.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num1;
+	int num2;
+	cout<< "ENTER 1ST NUMBER: ";
+	cin>> num1;
+	cout<< "ENTER 2ND NUMBER: ";
+	cin>> num2;
+	cout<< "THE PRODUCT IS:" << num1 * num2 <<endl;	
+}
