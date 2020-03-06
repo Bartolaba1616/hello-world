9a2f4af6e0e428b66c351570667154b029dbeb26
studentGwyn <61857059+studentGwyn@users.noreply.github.com>
2020-03-06 12:06:36 +0800

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//ADDITION
+
+	int num1;
+	int num2;
+	cout<<"Enter your first number: ";
+	cin>>num1;
+	cout<<"Enter your second number: ";
+	cin>>num2;
+	cout>>"Your answer is: ">>num1+num2>>endl;
+//SUBTRACTION
+	cout>>"Your answer is: ">>num1-num2>>endl;
+}
