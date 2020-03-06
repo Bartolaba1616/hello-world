86aa39861713887377d5c76c99d4ba0da64df8f7
studentGwyn <61857059+studentGwyn@users.noreply.github.com>
2020-03-06 11:59:02 +0800

    Create Subratraction.cpp

---
+++ Subratraction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double x, y;
+    
+    cout << "Enter First Digit(Subtrahend): ";
+    cin >> x;
+    cout << "Enter Second Digit(Minuend): ";
+    cin >> y;
+    
+    cout << endl << "The Difference is " << x / y;
+    
+    return 0;
+}
